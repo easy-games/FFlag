ac2d3c575eb74f4c23e7696624f2ad5f6c8b65db
Brooke Rhodes <brooke@gril.me>
2021-04-07 17:52:00 +0100

    Implement wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-export {};
+import { t } from "@rbxts/t";
+import SheetValues, { FFlagValueTypes, SheetManager } from "./SheetValues";
+
+export class FFlagManager {
+    public SheetManager: SheetManager;
+
+    constructor(SpreadId: string) {
+        this.SheetManager = new SheetValues(SpreadId);
+    }
+
+    GetFFlag<T extends FFlagValueTypes>(fflag: FFlag<T>) {
+        const value = this.SheetManager.Values[fflag.Name];
+        if (!fflag.Guard(value)) throw `Value "${value}" (${typeOf(value)}) for FFLag ${fflag.Name} is incorrect type`;
+
+        if (!value) return fflag.DefaultValue;
+        return value;
+    }
+}
+
+export class FFlag<T extends FFlagValueTypes> {
+    constructor(public Name: string, public Guard: t.check<T>, public DefaultValue: T) {}
+}
